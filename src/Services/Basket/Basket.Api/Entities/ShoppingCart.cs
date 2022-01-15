c6b2a180f4e3cbfb37632f84628edb1f58037e2b
m.mozafari <m.mozafari@DEVELOPER-10>
2022-01-15 12:45:56 +0330

    Add Redis Docker And Create Basket Entity

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new();
+
+        private ShoppingCart()
+        {
+            
+        }
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+
+                return totalprice;
+            }
+        }
+    }
+}
--- src/Services/Basket/Basket.Api/Startup.cs
+++ src/Services/Basket/Basket.Api/Startup.cs
@@ -25,7 +25,11 @@ namespace Basket.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = Configuration.GetValue<string>("CasheSettings:ConnectionString");
 
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
